2ff7351387f7a09b7acf9ba65169fdcd1706121c
LeonAdeoye <leon.adeoye@gmail.com>
2023-07-15 22:42:57 +0800

    Line Chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {TickerApp} from "./components/TickerApp";
+import {PriceChartApp} from "./components/PriceChartApp";
 
 function App() {
   return (
     <div className="App">
       <TickerApp/>
+      <PriceChartApp/>
     </div>
   );
 }
--- src/components/PriceChartApp.js
+++ src/components/PriceChartApp.js
@@ -1,12 +1,95 @@
 import * as React from 'react';
-import {useEffect, useMemo, useState, useRef, useCallback} from "react";
-import { AgGridReact} from "ag-grid-react";
+import {useState} from "react";
+import { AgChartsReact } from 'ag-charts-react';
 
 export const PriceChartApp = () =>
 {
-    return (
-        <>
-            Hello
-        </>
-    );
+    const [options, setOptions] = useState({data: data,
+        series: [
+            {
+                xKey: 'time',
+                yKey: 'bestAsk',
+                yName: 'Intraday Price',
+            },
+            {
+                xKey: 'time',
+                yKey: 'bestBid',
+                yName: 'Intraday Price',
+            },
+        ],
+        theme: { baseTheme:  'ag-default-dark'},
+        legend: {
+            enabled: false,
+        },
+    });
+
+    return <AgChartsReact options={options} />;
 };
+
+const data = [
+    {
+        time: '09:00',
+        bestAsk: 700,
+        bestBid: 600
+    },
+    {
+        time: '09:30',
+        bestAsk: 680,
+        bestBid: 620
+    },
+    {
+        time: '10:00',
+        bestAsk: 660,
+        bestBid: 630
+    },
+    {
+        time: '10:30',
+        bestAsk: 650,
+        bestBid: 640
+    },
+    {
+        time: '11:00',
+        bestAsk: 690,
+        bestBid: 610
+    },
+    {
+        time: '11:30',
+        bestAsk: 650,
+        bestBid: 620
+    },
+    {
+        time: '12:00',
+        bestAsk: 660,
+        bestBid: 660
+    },
+    {
+        time: '12:30',
+        bestAsk: 630,
+        bestBid: 640
+    },
+    {
+        time: '13:00',
+        bestAsk: 630,
+        bestBid: 650
+    },
+    {
+        time: '13:30',
+        bestAsk: 680,
+        bestBid: 620
+    },
+    {
+        time: '14:00',
+        bestAsk: 640,
+        bestBid: 660
+    },
+    {
+        time: '14:30',
+        bestAsk: 670,
+        bestBid: 630
+    },
+    {
+        time: '15:00',
+        bestAsk: 610,
+        bestBid: 690
+    }
+];
