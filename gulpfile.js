211b90b2aa593c4a0a97f4c6cf5b09c1bd17ff68
LeonAdeoye <leon.adeoye@gmail.com>
2023-08-31 17:06:14 +0800

    Added gulpfile.js comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('styles', () => {
+// Task to compile Sass to CSS
+gulp.task('styles', () =>
+{
+    // Select all Sass files within the specified directory and its subdirectories
     return gulp.src('./src/styles/sass/**/*.scss')
+        // Compile Sass to CSS; log errors if any
         .pipe(sass().on('error', sass.logError))
+        // Output the compiled CSS files to the specified destination directory
         .pipe(gulp.dest('./src/styles/css/'));
 });
 
+// Task to watch for changes in Sass files and trigger the 'styles' task
 gulp.task('watch', () => {
+    // Set up a watcher on Sass files
     gulp.watch('./src/styles/sass/**/*.scss', gulp.series('styles'));
 });
 
+// Default task that combines the 'styles' and 'watch' tasks
 gulp.task('default', gulp.series('styles', 'watch'));
 
