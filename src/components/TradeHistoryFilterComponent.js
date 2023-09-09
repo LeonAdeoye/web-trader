794bad3968bba9608842fcb3f33a81898d078fc8
LeonAdeoye <leon.adeoye@gmail.com>
2023-09-09 22:47:01 +0800

    filter component styling

--- src/components/TradeHistoryFilterComponent.js
+++ src/components/TradeHistoryFilterComponent.js
@@ -11,16 +11,20 @@ const TradeHistoryFilterComponent = ({buySkew, sellSkew}) =>
     return (
         <div>
             <FormControl>
-                <RadioGroup row name="position" value={filterDays} onChange={handleDayFilterChange}>
-                    {sellSkew > 0 ? <Typography sx={{ fontSize: '12px' }}>Sell Skew {sellSkew.toFixed(2)}:1</Typography> : ""}
-                    {buySkew > 0 ? <Typography sx={{ fontSize: '12px' }}>Buy Skew {buySkew.toFixed(2)}:1</Typography> : ""}
-                    <FormControlLabel value="1" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>Days: 1</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="5" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>5</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="10" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>10</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="30" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>30</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="60" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>60</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="90" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>90</Typography>}labelPlacement="start"/>
-                    <FormControlLabel value="180" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>180</Typography>}labelPlacement="start"/>
+                <RadioGroup className="filter-parent" row name="position" value={filterDays} onChange={handleDayFilterChange} style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className="filter-days">
+                        <FormControlLabel value="1" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>Days: 1</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="5" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>5</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="10" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>10</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="30" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>30</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="60" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>60</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="90" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>90</Typography>}labelPlacement="start"/>
+                        <FormControlLabel value="180" control={<Radio size="small" sx={{color: grey[100],'&.Mui-checked': { color: red[600] }}}/>} label={<Typography sx={{ fontSize: '12px' }}>180</Typography>}labelPlacement="start"/>
+                    </div>
+                    <div className="skews" style={{marginLeft: "80px"}}>
+                        {sellSkew > 0 ? <Typography sx={{ fontSize: '12px'}}>Sell Skew {sellSkew.toFixed(2)}:1</Typography> : ""}
+                        {buySkew > 0 ? <Typography sx={{ fontSize: '12px' }}>Buy Skew {buySkew.toFixed(2)}:1</Typography> : ""}
+                    </div>
                 </RadioGroup>
             </FormControl>
         </div>);
