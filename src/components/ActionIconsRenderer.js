9e778573c2a2b24c9a03465e823c77833a410d47
LeonAdeoye <leon.adeoye@gmail.com>
2023-10-09 15:44:01 +0800

    actions for client interest

--- src/components/ActionIconsRenderer.js
+++ src/components/ActionIconsRenderer.js
@@ -3,36 +3,31 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import AddBoxIcon from "@mui/icons-material/AddBox";
-import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import {Tooltip} from "@mui/material";
 
-const ActionIconsRenderer = (params) => {
-    const {blastId} = params.data;
-    const {handleAction} = params.context;
+const ActionIconsRenderer = ({data, context}) =>
+{
+    const {handleAction} = context;
 
     const ACTIONS = {
         DELETE: 'delete',
         UPDATE: 'update',
         CLONE: 'clone',
-        ADD: 'add',
-        PLAY: 'play'
+        ADD: 'add'
     };
 
     return (
         <div>
-            <Tooltip title="Play blast.">
-                <PlayCircleIcon onClick={() => handleAction(ACTIONS.PLAY, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            <Tooltip title="Delete existing row.">
+                <DeleteIcon onClick={() => handleAction(ACTIONS.DELETE, data)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
             </Tooltip>
-            <Tooltip title="Delete existing blast.">
-                <DeleteIcon onClick={() => handleAction(ACTIONS.DELETE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            <Tooltip title="Edit existing row.">
+                <EditIcon onClick={() => handleAction(ACTIONS.UPDATE, data)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
             </Tooltip>
-            <Tooltip title="Edit existing blast.">
-                <EditIcon onClick={() => handleAction(ACTIONS.UPDATE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            <Tooltip title="Clone existing row.">
+                <FileCopyIcon onClick={() => handleAction(ACTIONS.CLONE, data)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
             </Tooltip>
-            <Tooltip title="Clone existing blast.">
-                <FileCopyIcon onClick={() => handleAction(ACTIONS.CLONE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
-            </Tooltip>
-            <Tooltip title="Add new blast.">
+            <Tooltip title="Add new row.">
                 <AddBoxIcon onClick={() => handleAction(ACTIONS.ADD)} style={{cursor: 'pointer', color:'#404040', height:'20px'}}/>
             </Tooltip>
         </div>
---
+++ src/components/BlastActionIconsRenderer.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import {Tooltip} from "@mui/material";
+
+const BlastActionIconsRenderer = (params) => {
+    const {blastId} = params.data;
+    const {handleAction} = params.context;
+
+    const ACTIONS = {
+        DELETE: 'delete',
+        UPDATE: 'update',
+        CLONE: 'clone',
+        ADD: 'add',
+        PLAY: 'play'
+    };
+
+    return (
+        <div>
+            <Tooltip title="Play blast.">
+                <PlayCircleIcon onClick={() => handleAction(ACTIONS.PLAY, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            </Tooltip>
+            <Tooltip title="Delete existing blast.">
+                <DeleteIcon onClick={() => handleAction(ACTIONS.DELETE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            </Tooltip>
+            <Tooltip title="Edit existing blast.">
+                <EditIcon onClick={() => handleAction(ACTIONS.UPDATE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            </Tooltip>
+            <Tooltip title="Clone existing blast.">
+                <FileCopyIcon onClick={() => handleAction(ACTIONS.CLONE, blastId)} style={{cursor: 'pointer', marginRight: '5px', color:'#404040', height:'20px'}}/>
+            </Tooltip>
+            <Tooltip title="Add new blast.">
+                <AddBoxIcon onClick={() => handleAction(ACTIONS.ADD)} style={{cursor: 'pointer', color:'#404040', height:'20px'}}/>
+            </Tooltip>
+        </div>
+    );
+};
+
+export default BlastActionIconsRenderer;
