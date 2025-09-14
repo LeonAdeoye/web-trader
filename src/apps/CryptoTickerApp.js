3805da3d7d962810fe61af7952504140c0c3abfe
LeonAdeoye <leon.adeoye@gmail.com>
2025-09-14 14:50:47 +0800

    refactored crypto app

--- src/apps/CryptoTickerApp.js
+++ src/apps/CryptoTickerApp.js
@@ -19,7 +19,6 @@ export const CryptoTickerApp = () =>
     const configurationService = useRef(ServiceRegistry.getConfigurationService()).current;
     const marketDataService = useRef(ServiceRegistry.getMarketDataService()).current;
     const loggerService = useRef(new LoggerService(CryptoTickerApp.name)).current;
-    const gridDimensions = useMemo(() => ({ height: '100%', width: '100%' }), []);
     const defaultColDef = useMemo(() => ({resizable: true, filter: true, sortable: true}), []);
     const getRowId = useMemo(() => (row) => row.data.symbol, []);
     const windowId = useMemo(() => window.command.getWindowId("Crypto Ticker"), []);
@@ -122,29 +121,10 @@ export const CryptoTickerApp = () =>
             <TitleBarComponent title="Crypto Ticker" windowId={windowId} addButtonProps={undefined} showChannel={false} showTools={false}/>
             <div style={{ width: '100%', height: 'calc(100vh - 65px)', float: 'left', padding: '0px', margin:'45px 0px 0px 0px'}}>
                 <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' , padding: '0px', margin:'0px'}}>
-                    <AgGridReact
-                        ref={gridApiRef}
-                        columnDefs={columnDefs}
-                        rowData={prices}
-                        defaultColDef={defaultColDef}
-                        enableCellChangeFlash={true}
-                        rowSelection={'single'}
-                        onSelectionChanged={onSelectionChanged}
-                        animateRows={true}
-                        getRowId={getRowId}
-                        rowHeight={22}
-                        headerHeight={22}
-                    />
+                    <AgGridReact ref={gridApiRef} columnDefs={columnDefs} rowData={prices} defaultColDef={defaultColDef} enableCellChangeFlash={true} rowSelection={'single'}
+                         onSelectionChanged={onSelectionChanged} animateRows={true} getRowId={getRowId} rowHeight={22}  headerHeight={22}/>
                 </div>
             </div>
-            {errorMessage && (
-                <ErrorMessageComponent
-                    message={errorMessage}
-                    duration={10000}
-                    onDismiss={() => setErrorMessage(null)}
-                    position="bottom-right"
-                />
-            )}
-        </>
-    );
+            {errorMessage && (<ErrorMessageComponent message={errorMessage} duration={10000} onDismiss={() => setErrorMessage(null)} position="bottom-right"/>)}
+        </>);
 };
